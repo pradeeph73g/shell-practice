7040f776819c7421c11e8e1512c02c65079e62f9
pradeeph <pradeepd624@gmail.com>
2026-06-20 00:02:03 +0530

    conversation

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "pradeep:: Hai gosala this is my first shell script"
+echo "gosala:: hai pradeep, nice to meet you"
+echo "pradeep:: let's learn shell scripting together"
+echo "gosala:: sure, let's start learning shell scripting pradeep"
