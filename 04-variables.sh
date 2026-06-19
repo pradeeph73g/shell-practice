64822547a92a493f074ff9a3b2a24ef3105c1ba7
pradeeph <pradeepd624@gmail.com>
2026-06-20 00:05:45 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#No space before or after equal sign (=)
+PERSON1=kumar
+PERSON2=dizzy
+
+
+echo "$PERSON1:: Hai $PERSON2 this is my first shell script"
+echo "$PERSON2:: hai $PERSON1, nice to meet you, this is my first shell script"
+echo "$PERSON1:: let's learn shell scripting together"
+echo "$PERSON2:: sure, let's start learning shell scripting $PERSON1"
